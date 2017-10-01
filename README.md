d0aca3e4d7535362b5e94d1b8f424c105f762701
Siddharth Ajmera <siddajmera@outlook.com>
2017-10-01 17:39:32 +0530

    Implemented a webhook that would return the details of a movie.

---
+++ README.md
@@ -0,0 +1,6 @@
+# MovieDB Webhook - Get details of a movie from this webhook.
+
+A very simple webhook that gets you the details of a specified movie.
+
+# Deploy to:
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/get-movie-details', function (req, res) {
+
+    let movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movieToSearch ? req.body.result.parameters.movieToSearch : 'The Godfather';
+    let reqUrl = encodeURI('http://theapache64.xyz:8080/movie_db/search?keyword=' + movieToSearch);
+    http.get(reqUrl, (responseFromAPI) => {
+
+        responseFromAPI.on('data', function (chunk) {
+            let movie = JSON.parse(chunk)['data'];
+            let dataToSend = movieToSearch === 'The Godfather' ? 'I don\'t have the required info on that. Here\'s some info on \'The Godfather\' instead.\n' : '';
+            dataToSend += movie.name + ' is a ' + movie.stars + ' starer ' + movie.genre + ' movie, released in ' + movie.year + '. It was directed by ' + movie.director;
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-movie-details'
+            });
+
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-movie-details'
+        });
+    });
+});
+
+server.listen((process.env.PORT || 8000), function () {
+    console.log("Server is up and running...");
+});
