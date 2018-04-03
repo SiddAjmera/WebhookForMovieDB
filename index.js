d249306591afd65697266bb8f1f7708f997a9b24
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2018-04-03 14:28:20 +0530

    Made the callbacks consistent.

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('/get-movie-details', function (req, res) {
+server.post('/get-movie-details', (req, res) => {
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
-        responseFromAPI.on('data', chunk => completeResponse += chunk);
-        responseFromAPI.on('end', function () {
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
@@ -39,6 +41,6 @@ server.post('/get-movie-details', function (req, res) {
     });
 });
 
-server.listen((process.env.PORT || 8000), function () {
+server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
 });
