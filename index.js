8b401b132d414cad79271f9f2b8872426b30477c
Abdul Diaz <diazabdulm@gmail.com>
2018-05-06 21:20:25 -0400

    Converted to DialogFlow V2 format

--- index.js
+++ index.js
@@ -27,15 +27,13 @@ server.post('/get-movie-details', (req, res) => {
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
+                fulfillmentText: dataToSend,
                 source: 'get-movie-details'
             });
         });
     }, (error) => {
         return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
+            fulfillmentText: 'Something went wrong!',
             source: 'get-movie-details'
         });
     });
